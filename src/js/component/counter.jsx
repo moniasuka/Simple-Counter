09713a6285532617785646c90fccdd6f721c1b49
moniasuka <moniasuka@hotmail.com>
2022-09-29 02:36:37 +0000

    pushing counter exercise

---
+++ src/js/component/counter.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const Counter = (props) => {
+return(
+
+    
+
+        <div className="container text-center">
+            <div className="row align-items-center px-1 py-5 my-5 bg-dark">
+
+                <div className="col text-white border rounded border-secondary px-1 py-5 mx-2">
+                 <i class="far fa-clock fa-4x"></i>
+                </div>
+                <div className="col text-white display-4 border rounded border-secondary px-1 py-5 mx-2">
+                {props.tiempo6}
+                </div>
+                <div className="col text-white display-4 border rounded border-secondary px-1 py-5 mx-2">
+                {props.tiempo5}
+                </div>
+                <div className="col text-white display-4 border rounded border-secondary px-1 py-5 mx-2">
+                {props.tiempo4}
+                </div>
+                <div className="col text-white display-4 border rounded border-secondary px-1 py-5 mx-2">
+                {props.tiempo3} 
+                </div>
+                <div className="col text-white display-4 border rounded border-secondary px-1 py-5 mx-2">
+                {props.tiempo2}
+                </div>
+                <div className="col text-white display-4 border rounded border-secondary px-1 py-5 mx-2">
+                {props.tiempo}
+                </div>
+                <button className="bg-dark text-white display-1 my-2 border-secondary" type="button" onClick={()=>{clearInterval(props.stop)}}>Stop</button>
+            </div>   
+        </div>
+   
+
+   
+
+)
+};
--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,60 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import {Counter} from "./component/counter.jsx";
+
+let segundo = 0
+let minuto = 0
+let hora = 0
+
+let segundo2 = 0
+let minuto2= 0
+let hora2 = 0
+
+
+let timer = setInterval(()=> {
+console.log(segundo)
+if(segundo >= 10){
+    segundo=0
+    minuto++;
+}
+if(minuto >= 10)
+{
+    minuto=0
+    hora++;
+}
+
+if(hora >= 10)
+{
+    hora=0
+    segundo2++;
+}
+
+if(segundo2 >= 10)
+{
+    segundo2=0
+    minuto2++;
+}
+
+if(minuto2 >= 10)
+{
+    minuto2=0
+    hora2++;
+}
+
+
+ReactDOM.render(<Counter tiempo6={hora2} tiempo5={minuto2} tiempo4={segundo2} tiempo3={hora} tiempo2={minuto} tiempo={segundo} stop={timer}/>, document.querySelector("#app"));
+segundo ++; 
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+},1)
+
+
+
+
